4f5741af0d30cb5b83981fe32c3893a5d27d5531
Singh <taj.singh0715@gmail.com>
2020-07-09 04:50:17 +0200

    refacto

--- SmartCase_PA.ino
+++ SmartCase_PA.ino
@@ -11,9 +11,9 @@
 #define user "bluecase"
 #define mdp "bluecase"
 #define wifi_topic "WIFI/pa"
-
-bool debug = true;  //Affiche sur la console si True
-
+#define topic_lastWill "WIFI/death"
+#define port 15013
+#define client_id "fbc0dccd3aaa47a0b775dbff28ff3638"
 
 const int MPU_ADDR = 0x68; // adresse I2C du MPU.
 
@@ -21,15 +21,17 @@ const int MPU_ADDR = 0x68; // adresse I2C du MPU.
 int16_t accelerometer_x, accelerometer_y, accelerometer_z; // variables pour l'acceloremetre
 int16_t gyro_x, gyro_y, gyro_z; // variable pour le gyro 
 
-char new_ssid[50] ;
+//Permet de stocker les nouveaux ssid et password depuis la lib wifimanager.
+char new_ssid[50]; 
 char new_password[50];
 
 WiFiManager wm;
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+//Reseau wifi en AP de l'esp utile pour le setup de l'arduino.
 const char* ssid = "SmartCase";
-const char* password = "SmartCase"; //TODO - Voir pour pouvoir se connecter directement depuis le tel ? - Fait !
+const char* password = "SmartCase";
 
 void setup_wifi(char* ssid, char* password) {
   delay(10);
@@ -42,7 +44,6 @@ void setup_wifi(char* ssid, char* password) {
     Serial.print("Connexion a ");
     Serial.println(ssid);
   }
-
   Serial.println("");
   Serial.println("Connexion WiFi etablie ");
   Serial.print("=> Addresse IP : ");
@@ -50,10 +51,10 @@ void setup_wifi(char* ssid, char* password) {
 }
 
 void reconnect() {
-  //Boucle jusqu'à obtenur une reconnexion
+  //Boucle jusqu'à obtenir une reconnexion
   while (!client.connected()) {
     Serial.print("Connexion au serveur MQTT...");
-    if (client.connect("fbc0dccd3aaa47a0b775dbff28ff3638", user, mdp, "WIFI/death", 0, false, "Je suis mort")) {
+    if (client.connect(client_id, user, mdp, topic_lastWill, 0, false, "Je suis mort")) {
       Serial.println("OK");
     } else {
       client.setKeepAlive(5);
@@ -82,25 +83,10 @@ void setup() {
     setup_wifi(new_ssid, new_password);
   }
   
-    client.setServer(mqtt_server, 15013);    //Configuration de la connexion au serveur MQTT
-    client.setKeepAlive(5);
-    //Serial.println(wm.getWiFiPass());
-    //Serial.println(wm.getWiFiSSID());
-    //Test websocket du jeudi 02/07   
-
-  /*webSocketClient.path = path;
-  webSocketClient.host = host;
-  if (webSocketClient.handshake(client)) {
-    Serial.println("Handshake successful");
-  } else {
-    Serial.println("Handshake failed.");
-  }*/
-
-  //wifiServer.begin();
-  //ws.onEvent(onWsEvent);
-  //server.addHandler(&ws);
-  //server.begin();
-
+  client.setServer(mqtt_server, port);    //Configuration de la connexion au serveur MQTT
+  client.setKeepAlive(5);
+  //Serial.println(wm.getWiFiPass());
+  //Serial.println(wm.getWiFiSSID()); 
 }
 
 void loop() {
@@ -111,30 +97,32 @@ void loop() {
   
   client.loop();
   //Logique accéleromètre 
-  /*Wire.beginTransmission(MPU_ADDR);
-  Wire.write(0x3B); //Envoi de données vers la carte maitre
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU_ADDR, 7*2, true); //La carte I2C demande 14 registres au GY-521.*/
+  /*
+   *Wire.beginTransmission(MPU_ADDR);
+    Wire.write(0x3B); //Envoi de données vers la carte maitre
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU_ADDR, 7*2, true); //La carte I2C demande 14 registres au GY-521.
+   */
   
   // "Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
   // On divise par 16384 pour avoir une mesure à +-2g (gravité).
  
   /*
-   accelerometer_x = Wire.read()<<8 | Wire.read() / 16384;
-  accelerometer_y = Wire.read()<<8 | Wire.read() / 16384;
-  accelerometer_z = Wire.read()<<8 | Wire.read() / 16384;
+   *accelerometer_x = Wire.read()<<8 | Wire.read() / 16384;
+    accelerometer_y = Wire.read()<<8 | Wire.read() / 16384;
+    accelerometer_z = Wire.read()<<8 | Wire.read() / 16384;
 
   //On divise par 131 pour avoir une portée de 250°
-  gyro_x = Wire.read()<<8 | Wire.read() / 131; 
-  gyro_y = Wire.read()<<8 | Wire.read() / 131; 
-  gyro_z = Wire.read()<<8 | Wire.read() / 131;
+    gyro_x = Wire.read()<<8 | Wire.read() / 131; 
+    gyro_y = Wire.read()<<8 | Wire.read() / 131; 
+    gyro_z = Wire.read()<<8 | Wire.read() / 131;
   
 
-  Serial.print("aX = "); Serial.print(accelerometer_x);
-  Serial.print(" | aY = "); Serial.print(accelerometer_y);
-  Serial.print(" | aZ = "); Serial.print(accelerometer_z);
-
-  Serial.println();
+    Serial.print("aX = "); Serial.print(accelerometer_x);
+    Serial.print(" | aY = "); Serial.print(accelerometer_y);
+    Serial.print(" | aZ = "); Serial.print(accelerometer_z);
+  
+    Serial.println();
   */
   
   //Logique puissance du WiFi
@@ -148,7 +136,6 @@ void loop() {
   
   client.publish(wifi_topic, String(rssi).c_str(), false);
 
-  
   // delay
   delay(2000);
 }
